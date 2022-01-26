ffebc43869068354b10b4176e1d4c8a83653f586
jojo3420 <jjjhhhvvv@gmail.com>
2022-01-26 10:09:53 +0900

    requirments update  - install scheduler

--- main.py
+++ main.py
@@ -5,18 +5,7 @@ import datetime as dt
 from scheduler import Scheduler
 import scheduler.trigger as trigger
 
-
-# debug('프로그램시작')
-# schedule.every(1).minutes.do(run)  # 1분마다 실행
-# schedule.every().day.at("10:00").do(run)  # 매일 10시00분
-
-# 실제 실행하게 하는 코드
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
-
-
-
+debug('프로그램시작')
 
 TZ_KST = dt.timezone(dt.timedelta(hours=9))
 schedule = Scheduler(tzinfo=dt.timezone.utc)
